0e16c8625362236bf512acfa89f2cafdee14876b
inkdog55 <inkdog55@gmail.com>
2023-12-06 06:52:01 +0900

    배포 위해 설정파일 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isProduction = process.env.NODE_ENV === "production";
+const repository = "practice-parallax";
+
+const nextConfig = {
+  assetPrefix: isProduction ? `/${repository}/` : "", // production 일때 prefix 경로
+  output: "export",
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
